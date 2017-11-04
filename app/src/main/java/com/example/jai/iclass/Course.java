71d950c7c572e95de4ff6b547449844a86bdb674
Jaivardhan <jvss1996@gmail.com>
2017-11-04 20:53:51 +0530

    First Commit

---
+++ app/src/main/java/com/example/jai/iclass/Course.java
@@ -0,0 +1,48 @@
+package com.example.jai.iclass;
+
+import java.util.ArrayList;
+
+public class Course {
+
+
+    private String courseKey;
+    private String courseName;
+    private String department;
+    private String facultyName;
+    private String description;
+
+
+    public Course() {
+    }
+
+    public Course(String courseKey,String courseName, String department, String facultyName, String description) {
+
+        this.courseKey = courseKey;
+        this.courseName = courseName;
+        this.department = department;
+        this.facultyName = facultyName;
+        this.description = description;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getFacultyName() {
+        return facultyName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCourseKey() {
+        return courseKey;
+    }
+
+
+}
